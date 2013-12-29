09876404c326eed1225dab6786ef1529b8aa81af
allenhu <thefivefire@gmail.com>
2013-12-29 17:08:02 +0800

    修改错误的pypi地址

--- essay/__init__.py
+++ essay/__init__.py
@@ -1,9 +1,8 @@
 from fabric.state import env
 
-env.GIT_SERVER = '10.10.51.78'
-env.DEPLOY_HOST = '10.13.83.204'
-env.DEPLOY_USER = 'moon'
+env.GIT_SERVER = 'https://github.com/'
+env.DEPLOY_USER = 'noroot'
 env.DEPLOY_PATH = '/opt/deploy/'
-env.PROJECT_OWNER = 'clan'
+env.PROJECT_OWNER = 'SohuTech'
 env.DEFAULT_BRANCH = 'master'
-env.PYPI_INDEX = 'http://10.13.83.194:9791/simple/'
+env.PYPI_INDEX = 'http://pypi.douban.com/simple/'
