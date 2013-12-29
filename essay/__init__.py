afb3337f850bda197d928800ef78c6c3c70bcd6e
allenhu <thefivefire@gmail.com>
2013-12-29 17:10:05 +0800

    去掉会引起歧义的配置

--- essay/__init__.py
+++
@@ -1,8 +0,0 @@
-from fabric.state import env
-
-env.GIT_SERVER = 'https://github.com/'
-env.DEPLOY_USER = 'noroot'
-env.DEPLOY_PATH = '/opt/deploy/'
-env.PROJECT_OWNER = 'SohuTech'
-env.DEFAULT_BRANCH = 'master'
-env.PYPI_INDEX = 'http://pypi.douban.com/simple/'
